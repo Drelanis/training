a95c02e961afe85174e59aecae4ea50304fe32c8
Drelanis <denisbadakaty51@gmail.com>
2023-04-18 15:55:49 +0300

    1

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-import './styles.css';
-import React from 'react';
-import { render } from 'react-dom';
-
 const rootElement = document.querySelector('#root');
 
 const greetingElement = React.createElement('div', { className: 'greeting' }, 'Hello, React!');
