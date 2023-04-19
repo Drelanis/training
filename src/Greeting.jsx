93019b9c1780b5c203618c92a2daa1b7f414c0fa
Drelanis <denisbadakaty51@gmail.com>
2023-04-19 22:39:50 +0300

    1

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const getYearsOld = birthDate => new Date().getUTCFullYear() - birthDate;
-
 const Greeting = props => {
+  const getYearsOld = birthDate => new Date().getUTCFullYear() - birthDate;
+
   return (
     <div className="greeting">
       My name is {props.firstName} {props.lastName}. I am {getYearsOld(props.birthDate)} years old
