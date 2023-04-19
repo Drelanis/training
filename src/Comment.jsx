2cec44ebe96db00e8f2be84f2aecac5cd475d50a
Drelanis <denisbadakaty51@gmail.com>
2023-04-19 21:54:46 +0300

    1

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './comment.scss';
 import UserInfo from './UserInfo.jsx';
+import moment from 'moment';
+
+const formatDate = date => moment(date).format('DD MMM YYYY');
 
 const Comment = props => {
   return (
     <div className="comment">
-      <UserInfo
-        avatarUrl={props.author.avatarUrl}
-        name={props.author.name}
-        date={props.author.date}
-      />
+      <UserInfo author={props.author} />
+      <div className="comment__text">{props.text}</div>
+      <div className="comment__date">{formatDate(props.date)}</div>
     </div>
   );
 };
--- src/UserInfo.jsx
+++ src/UserInfo.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Avatar from './Avatar.jsx';
 import './userInfo.scss';
-import moment from 'moment';
-
-const formatDate = date => moment(date).format('DD MMM YYYY');
 
 const UserInfo = props => {
   return (
     <>
       <div className="user-info">
-        <Avatar avatarUrl={props.avatarUrl} name={props.name} />
-        <div className="user-info__name">{props.name}</div>
+        <Avatar avatarUrl={props.author.avatarUrl} name={props.author.name} />
+        <div className="user-info__name">{props.author.name}</div>
       </div>
-      <div className="comment__text">{props.text}</div>
-      <div className="comment__date">{formatDate(props.date)}</div>
     </>
   );
 };
