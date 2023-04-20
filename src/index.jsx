a482bd707de0a6825c970118600c6571ea7db72b
Drelanis <denisbadakaty51@gmail.com>
2023-04-20 15:10:25 +0300

    1

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import './Profile.jsx';
-import Profile from './Profile.jsx';
+import App from './App.jsx';
 
 const rootElement = document.querySelector('#root');
 
-const userData = {
-  firstName: 'James',
-  lastName: 'Bond',
-  birthDate: '1991-01-17T11:11:11.819Z',
-  birthPlace: 'London',
-};
-
-ReactDOM.render(<Profile userData={userData} />, rootElement);
+ReactDOM.render(<App />, rootElement);
