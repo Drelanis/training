ab0ba0c213dcc4c3fb9e3b8d9661e61875fdb9aa
Drelanis <denisbadakaty51@gmail.com>
2023-04-21 13:09:48 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import Colors from './Colors.jsx';
 
-const RED = '#ff0000';
-const GREEN = '#008000';
-const BLUE = '#0000ff';
-
 const App = () => {
   return (
     <div className="colors">
-      <Colors color={RED} />
-      <Colors color={GREEN} />
-      <Colors color={BLUE} />
+      <Colors />
     </div>
   );
 };
--- src/Colors.jsx
+++ src/Colors.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+const RED = '#ff0000';
+const GREEN = '#008000';
+const BLUE = '#0000ff';
+
 class Colors extends Component {
   constructor(props) {
     super(props);
@@ -14,11 +18,23 @@ class Colors extends Component {
 
   render() {
     return (
-      <div
-        className="colors__button"
-        style={{ backgroundColor: this.state.color }}
-        onClick={this.setBackgroundColor}
-      ></div>
+      <>
+        <div
+          className="colors__button"
+          style={{ backgroundColor: RED }}
+          onClick={this.setBackgroundColor}
+        ></div>
+        <div
+          className="colors__button"
+          style={{ backgroundColor: GREEN }}
+          onClick={this.setBackgroundColor}
+        ></div>
+        <div
+          className="colors__button"
+          style={{ backgroundColor: BLUE }}
+          onClick={this.setBackgroundColor}
+        ></div>
+      </>
     );
   }
 }
