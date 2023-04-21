9384efb490a3b62543afb08af6bfec24ac7cbc32
Drelanis <denisbadakaty51@gmail.com>
2023-04-21 11:57:43 +0300

    1

--- src/GoodButton.jsx
+++ src/GoodButton.jsx
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 
-class GoodButton extends Component {
-  showText = event => {
-    alert(event.target.textContent);
-  };
-
-  render() {
-    return (
-      <button className="fancy-button" onClick={event => this.showText(event)}>
-        Click me!
-      </button>
-    );
-  }
-}
+const GoodButton = () => {
+  return (
+    <button className="fancy-button" onClick={() => alert('Good job!')}>
+      Click me!
+    </button>
+  );
+};
 
 export default GoodButton;
