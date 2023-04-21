f374dd02e50cbbd5ec00d8f0946f0d66b9b39a58
Drelanis <denisbadakaty51@gmail.com>
2023-04-21 11:31:35 +0300

    1

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from '../App.jsx';
+import App from './App.jsx';
 import './index.scss';
-import GoodButton from './GoodButton.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<GoodButton />, rootElement);
+ReactDOM.render(<App />, rootElement);
