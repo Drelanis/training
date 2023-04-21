bb8ae16d6c1b95831aa55f65196a05a899de28a9
Drelanis <denisbadakaty51@gmail.com>
2023-04-21 15:28:57 +0300

    1

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -9,13 +9,8 @@ class ColorPicker extends Component {
     return document.querySelector('.picker__title');
   }
 
-  renderColor = event => {
-    if (event.target.className.split(' ').includes('picker__button_coral'))
-      this.pickerTitle().textContent = 'Coral';
-    if (event.target.className.split(' ').includes('picker__button_aqua'))
-      this.pickerTitle().textContent = 'Aqua';
-    if (event.target.className.split(' ').includes('picker__button_bisque'))
-      this.pickerTitle().textContent = 'Bisque';
+  renderColor = color => {
+    this.pickerTitle().textContent = `${color}`;
   };
 
   reset = () => {
@@ -29,18 +24,18 @@ class ColorPicker extends Component {
         <div>
           <button
             className="picker__button picker__button_coral"
-            onMouseEnter={this.renderColor}
-            onMouseOut={this.reset}
+            onMouseEnter={() => this.renderColor('Coral')}
+            onMouseLeave={this.reset}
           ></button>
           <button
             className="picker__button picker__button_aqua"
-            onMouseEnter={this.renderColor}
-            onMouseOut={this.reset}
+            onMouseEnter={() => this.renderColor('Aqua')}
+            onMouseLeave={this.reset}
           ></button>
           <button
             className="picker__button picker__button_bisque"
-            onMouseEnter={this.renderColor}
-            onMouseOut={this.reset}
+            onMouseEnter={() => this.renderColor('Bisque')}
+            onMouseLeave={this.reset}
           ></button>
         </div>
       </div>
