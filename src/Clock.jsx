c226028cd250e969a6a4083b05887a3bb3a19fb5
Drelanis <denisbadakaty51@gmail.com>
2023-05-06 13:42:41 +0300

    1

---
+++ src/Clock.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: props.location,
+      offset: moment(getTimeWithOffset(props.offset)).format('h:mm:ss A'),
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        offset: moment(getTimeWithOffset(this.props.offset)).format('h:mm:ss A'),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__location">{this.state.location}</div>
+        <div className="clock__time">{this.state.offset}</div>
+      </div>
+    );
+  }
+}
+
+export default Clock;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Life from './Life.jsx';
+import App from './App.jsx';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Life />, rootElement);
+ReactDOM.render(<App />, rootElement);
